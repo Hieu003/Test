27095365928c7a58632f5ba668013e8a0fd0679e
Hieu003 <dominhhieu003@gmail.com>
2024-06-15 10:37:54 +0700

    first component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import { useDispatch, useSelector } from 'react-redux';
 import { increaseCounter, decreaseCounter } from './redux/action/counterAction';
+import MyComponent from './Components/MyComponent';
 const App = () => {
   const count = useSelector(state => state.counter.count);
   const dispatch = useDispatch();
@@ -10,6 +11,7 @@ const App = () => {
 
     <div>
       Hello world
+      <MyComponent></MyComponent>
     </div>
     // <div className="App">
     //   <header className="App-header">
