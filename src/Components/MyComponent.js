87818ced21814a35d49aac3a90e1da70ad96f33b
Hieu003 <dominhhieu003@gmail.com>
2024-06-15 10:53:23 +0700

    state

--- src/Components/MyComponent.js
+++ src/Components/MyComponent.js
@@ -1,12 +1,18 @@
 import React from "react";
 
 
+
+
 class MyComponent extends React.Component {
+
+    state = {
+        name: "Hieu",
+        address: "Ninh Binh"
+    };
+
     render() {
         return (
-            <div>My First Component
-
-                {Math.random()}
+            <div> Hello my name is {this.state.name} and im from {this.state.address}
             </div>
         )
     }
