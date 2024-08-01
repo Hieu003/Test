d2b23b7aa37571f606c0a197b876e9c786018333
Hieu003 <dominhhieu003@gmail.com>
2024-08-01 19:24:06 +0700

    data ques

--- src/Components/User/DetailQuiz.js
+++ src/Components/User/DetailQuiz.js
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
 import { useParams } from "react-router-dom"
 import { getDataQuiz } from "../Service/apiService";
+import _, { values } from 'lodash'
 
 const DetailQuiz = (props) => {
     const params = useParams()
@@ -13,6 +14,25 @@ const DetailQuiz = (props) => {
     const fetchQuestions = async () => {
         let res = await getDataQuiz(quizId);
         console.log("check question", res);
+        if (res && res.EC) {
+            let raw = res.DT
+            let data = _.chain(raw)
+                .groupBy("id")
+                .map((value, key) => {
+                    let answers = []
+                    let questionDescription, image = null
+                    value.forEach((item, index) => {
+                        if (index === 0) {
+                            questionDescription = item.description;
+                            image = item.image
+                        }
+                        answers.push(item.answers);
+
+                    })
+                    return { questionId: key, answers, questionDescription, image }
+                })
+                .value();
+        }
     }
     return (
         <span>DetailQuiz</span>
